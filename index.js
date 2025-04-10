df2f437c1bf134438058a07649ebe75862fae492
yRtgXz <rtgzinmp3@email.com>
2025-04-10 15:19:11 -0300

    Corrigido uso de token via process.env

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
-const Discord = require("discord.js")
-
-const config = require("./config.json")
+const Discord = require("discord.js");
+require("dotenv").config();
 
 const client = new Discord.Client({ 
   intents: [ 
     Discord.GatewayIntentBits.Guilds,
     Discord.GatewayIntentBits.GuildMessages,
     Discord.GatewayIntentBits.MessageContent,
-    Discord.GatewayIntentBits.GuildMembers,
-    '32767'
-       ]
-    });
+    Discord.GatewayIntentBits.GuildMembers
+  ]
+});
 
-module.exports = client
+module.exports = client;
 
-client.on('interactionCreate', (interaction) => {
+client.slashCommands = new Discord.Collection();
 
-  if(interaction.type === Discord.InteractionType.ApplicationCommand){
+require('./handler')(client);
 
-      const cmd = client.slashCommands.get(interaction.commandName);
+client.on("interactionCreate", async (interaction) => {
+  if (interaction.type === Discord.InteractionType.ApplicationCommand) {
+    const cmd = client.slashCommands.get(interaction.commandName);
+    if (!cmd) return interaction.reply(`Error`);
+    interaction["member"] = interaction.guild.members.cache.get(interaction.user.id);
+    cmd.run(client, interaction);
+  }
 
-      if (!cmd) return interaction.reply(`Error`);
-
-      interaction["member"] = interaction.guild.members.cache.get(interaction.user.id);
-
-      cmd.run(client, interaction)
-
-   }
-})
+  require('./events/config-ticket').execute(interaction, client);
+  require('./events/ticket').execute(interaction, client);
+  require('./events/gerenciar').execute(interaction, client);
+});
 
 client.on('ready', () => {
-  console.log(`🔥 Estou online em ${client.user.username}!`)
-})
+  console.log(`🔥 Estou online em ${client.user.username}!`);
+});
 
-
-client.slashCommands = new Discord.Collection()
-
-require('./handler')(client)
-
-client.login(config.token)
-
-client.on("interactionCreate", require('./events/config-ticket').execute);
-client.on("interactionCreate", require('./events/ticket').execute);
-client.on("interactionCreate", require('./events/gerenciar').execute);
+client.login(process.env.TOKEN);
